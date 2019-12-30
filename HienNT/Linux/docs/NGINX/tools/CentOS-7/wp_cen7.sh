e4d6db7f3116f22840c832b6edecc5ffc24daff8
Nguyễn Thúy Hiền <48944577+thuyhiend@users.noreply.github.com>
2019-12-30 20:47:09 +0700

    Update wp_cen7.sh

--- HienNT/Linux/docs/NGINX/tools/CentOS-7/wp_cen7.sh
+++ HienNT/Linux/docs/NGINX/tools/CentOS-7/wp_cen7.sh
@@ -2,11 +2,18 @@
 
 ##### Script install Wordpress
 
+	### Variable default
+
+	dbname = wordpress
+	dbuser = wordpress
+	dbpass = wordpress
+	path = $path
+
 # Function check user root
 f_check_root () {
 	if(( $EUID == 0)); then
 		#if user is root, continue to function f_sub_main
-		f_create_database
+		f_sub_main
 	else
 		#if user not root, print message and exit script
 		echo "Please run this script by user root!"
@@ -14,14 +21,37 @@ f_check_root () {
 	fi
 }
 
-# Function create Databae
+# Function download Wordpress
+f_download_wordpress () {
+	yum install -y tar
+	echo "Enter Wordpress version you want to install(3/4/5): "
+	read version
+	if [[ "$version" == '3' ]]; then
+
+	wget -P $path/ https://wordpress.org/wordpress-3.9.30.tar.gz
+	tar -zxvf $path/wordpress-3.9.30.tar.gz -C $path/
+	rm -rf $path/wordpress-3.9.30.tar.gz
+
+	elif [[ "$version" == '4' ]]; then
+
+	wget -P $path/ https://wordpress.org/wordpress-4.9.13.tar.gz
+	tar -zxvf $path/wordpress-4.9.13.tar.gz -C $path/
+	rm -rf $path/wordpress-4.9.13.tar.gz
+	elif [[ "$version"  == '5' ]]; then
+	
+	wget -P $path/ https://wordpress.org/wordpress-5.3.2.tar.gz
+	tar -zxvf $path/wordpress-5.3.2.tar.gz -C $path/
+	rm -rf $path/wordpress-5.3.2.tar.gz
+
+	else
+
+	echo "Script don't support this version!"
+	fi
+}
+
+# Function create database
 f_create_database (){
-	echo "Enter Database: "
-	read dbname
-	echo "Enter User Database: "
-	read dbuser
-	echo "Enter Password User Database: "
-	read dbpass
+
 	mysql -uroot -p<<MYSQL_SCRIPT
 	CREATE DATABASE $dbname;
 	CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$dbpass';
@@ -29,59 +59,32 @@ f_create_database (){
 	FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
-	echo "DATABASE is created. Install Wordpress now? (y/n)"
-	read run
-	if [[ "$run" = "n" ]]; then
-		exit
-	else
-		f_sub_main
-	fi
+	echo "Installing Wordpress..."
 }
-
-
-# Function download Wordpress
-f_download_wordpress () {
-	yum install -y tar
-	echo "Enter Wordpress version you want to install(3/4/5): "
-	read version
-	if [[ "$version" -eq '3' ]]; then
-		wget -P /usr/share/nginx/html/ https://wordpress.org/wordpress-3.9.30.tar.gz
-		tar -zxvf /usr/share/nginx/html/wordpress-3.9.30.tar.gz -C /usr/share/nginx/html/
-		rm -rf /usr/share/nginx/html/wordpress-3.9.30.tar.gz
-	elif [[ "$version" -eq '4' ]]; then
-		wget -P /usr/share/nginx/html/ https://wordpress.org/wordpress-4.9.13.tar.gz
-		tar -zxvf /usr/share/nginx/html/wordpress-4.9.13.tar.gz -C /usr/share/nginx/html/
-		rm -rf /usr/share/nginx/html/wordpress-4.9.13.tar.gz
-	elif [[ "$version" -eq '5' ]]; then
-		wget -P /usr/share/nginx/html/ https://wordpress.org/wordpress-5.3.2.tar.gz
-		tar -zxvf /usr/share/nginx/html/wordpress-5.3.2.tar.gz -C /usr/share/nginx/html/
-		rm -rf /usr/share/nginx/html/wordpress-5.3.2.tar.gz
-	else 
-		echo "Script don't support this version!"
-	fi
-}
-
 	
 # Function config Wordpress
 f_config_wp () {
+
 	### Find all of the directories and files in the defined path and update octal permissions.
 
-	chown -R nginx:nginx /usr/share/nginx/html/wordpress
-	find /usr/share/nginx/html/wordpress -type d -exec chmod 755 {} \;
-	find /usr/share/nginx/html/wordpress -type f -exec chmod 644 {} \;
+	chown -R nginx:nginx $path/wordpress
+	find $path/wordpress -type d -exec chmod 755 {} \;
+	find $path/wordpress -type f -exec chmod 644 {} \;
 	
 	### Create wp config
 
-	cp /usr/share/nginx/html/wordpress/wp-config-sample.php /usr/share/nginx/html/wordpress/wp-config.php
-	chown nginx:nginx /usr/share/nginx/html/wordpress/wp-config.php
+	cp $path/wordpress/wp-config-sample.php $path/wordpress/wp-config.php
+	chown nginx:nginx $path/wordpress/wp-config.php
 	
 	### Set database details with perl find and replace
 
-	sed -i "s/database_name_here/$dbname/g" /usr/share/nginx/html/wordpress/wp-config.php
-	sed -i "s/username_here/$dbuser/g" /usr/share/nginx/html/wordpress/wp-config.php
-	sed -i "s/password_here/$dbpass/g" /usr/share/nginx/html/wordpress/wp-config.php
+	sed -i "s/database_name_here/$dbname/g" $path/wordpress/wp-config.php
+	sed -i "s/username_here/$dbuser/g" $path/wordpress/wp-config.php
+	sed -i "s/password_here/$dbpass/g" $path/wordpress/wp-config.php
 	echo "listen = /var/run/php-fpm/php-fpm.sock" >> /etc/php-fpm.d/www.conf
 }
+
+
 
 # Function restart service
 f_restart_service () {
@@ -95,6 +98,7 @@ f_restart_service () {
 # The sub main function, use to call neccessary functions of installation
 f_sub_main () {
 	f_download_wordpress
+	f_create_database
 	f_config_wp
 	f_restart_service
 
@@ -102,5 +106,9 @@ f_sub_main () {
     sleep 1
 }
 
-f_check_root
+# The main dunction
+f_main () {
+	f_check_root
+}
+f_main
 exit
