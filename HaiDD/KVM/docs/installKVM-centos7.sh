be3ff441839d6281b21def664a9f27d14524fb7e
Đặng Đỗ Hải <danghai.hy.1996@gmail.com>
2020-01-12 23:31:42 +0700

    Update installKVM-centos7.sh

--- HaiDD/KVM/docs/installKVM-centos7.sh
+++ HaiDD/KVM/docs/installKVM-centos7.sh
@@ -13,6 +13,7 @@ f_check_os(){
         OS="CentOS_"
         if [ $(rpm --eval '%{centos_ver}') == 7 ]; then {
             OS="${OS}7";
+            echo $OS
             return 0
         } else {
             return 1
@@ -24,7 +25,7 @@ f_check_os(){
 
 # Check virt-support
 f_check_virt_sup() {
-    if [$(egrep -c "svm|vmx" /proc/cpuinfo) == 0] > /dev/null 2>&1; then {
+    if [ $(egrep -c "svm|vmx" /proc/cpuinfo) == '0' ] > /dev/null 2>&1; then {
         echo "System not suport Virtualization !!!";
         return 1
     } else {
@@ -55,12 +56,48 @@ f_install_xwindow() {
     yum install "@X Window System" xorg-x11-xauth xorg-x11-fonts-* xorg-x11-utils -y
 }
 
+# Check exist_name for card bridge
+check_exist_name () {
+    check=1
+    while [ $check -eq 1 ]
+    do 
+        # Enter and Standardized ifname for card bridge.
+        echo -n "ENTER Interface_name (For example: br0, br1, ...):"
+        read if_name
+        if_name=$(echo $if_name | tr -d ' ')
+        
+        # Check name
+        if [ $(nmcli device | grep -wc "^$if_name") == 0 ] > /dev/null 2>&1; then {
+            # Enter and Standardized con-name for card bridge
+            echo -n "ENTER Connection_name (For example: br0, br1, ...):"
+            read con_name
+            con_name=$(echo $con_name | tr -d ' ')
+            if [ $(nmcli con show | grep -wc "^$con_name") == 0 ] > /dev/null 2>&1; then {
+                echo "OK"
+                check=0
+                break
+            } else {
+                echo "PLEASE ENTER INTERFACE NAME AND CONNECTION NAME AGAIN...."
+                check=1
+            } fi
+        } else {
+            check=1
+        } fi
+    done
+    
+    return 0
+    echo "Continue......"
+}
 
-############################################
-############################################
-########             MAIN           ########
-############################################
-############################################
+# Add and configure card bridge
+#f_conf_network() {
+#
+#}
+
+
+#--------------------------------------------------------------#
+#-----------------------------MAIN-----------------------------#
+#--------------------------------------------------------------#
 
 f_main() {
     if f_check_os; then {
@@ -80,4 +117,9 @@ f_main() {
     } fi
 }
 
+
+
+#-------------RUN-------------#
 f_main
+
+exit
