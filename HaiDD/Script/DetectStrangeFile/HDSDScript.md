301294c019a62ba958f3f35029de7f6defcb7be1
Đặng Đỗ Hải <danghai.hy.1996@gmail.com>
2020-05-23 10:07:52 +0700

    add hdsd

---
+++ HaiDD/Script/DetectStrangeFile/HDSDScript.md
@@ -0,0 +1,2 @@
+# Hướng dẫn sử dụng Script Detect Strange Files
+
---
+++ HaiDD/Script/SSHLoginMonitor/sshLoginMonitor.sh
@@ -0,0 +1,40 @@
+# ID chat Telegram
+USERID="<target_user_id>"
+    
+# API Token bot
+TOKEN="<bot_private_TOKEN>"
+
+TIMEOUT="10"
+
+# URL gửi tin nhắn của bot
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+
+# Thời gian hệ thống
+DATE_EXEC="$(date "+%d %b %Y %H:%M")"
+
+# File temp
+TMPFILE='/tmp/ipinfo-$DATE_EXEC.txt'
+
+if [ -n "$SSH_CLIENT" ]; then
+    IP=$(echo $SSH_CLIENT | awk '{print $1}')
+    PORT=$(echo $SSH_CLIENT | awk '{print $3}')
+    HOSTNAME=$(hostname -f)
+    IPADDR=$(hostname -I | awk '{print $1}')
+
+    # Lấy các thông tin từ IP người truy cập theo trang ipinfo.io
+    curl http://ipinfo.io/$IP -s -o $TMPFILE
+    CITY=$(cat $TMPFILE | jq '.city' | sed 's/"//g')
+    REGION=$(cat $TMPFILE | jq '.region' | sed 's/"//g')
+    COUNTRY=$(cat $TMPFILE | jq '.country' | sed 's/"//g')
+    ORG=$(cat $TMPFILE | jq '.org' | sed 's/"//g')
+
+    # Nội dung cảnh báo
+    TEXT=$(echo -e "Thời gian: $DATE_EXEC\nUser: ${USER} logged in to $HOSTNAME($IPADDR) \nFrom $IP - $ORG - $CITY, $REGION, $COUNTRY on port $PORT")
+
+    # Gửi cảnh báo
+    curl -s -X POST --max-time $TIMEOUT $URL -d "chat_id=$USERID" -d text="$TEXT" > /dev/null
+
+    # Xóa file temp khi script thực hiện xong
+    rm $TMPFILE
+    
+fi
